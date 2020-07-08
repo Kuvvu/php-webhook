e513d098f9dfcfa2aca72e7ccf8c903edeace4f8
Simon Jenny <simon.jenny@me.com>
2020-07-08 10:50:01 +0200

    Mails and More

---
+++ src/Helper/Mail.php
@@ -0,0 +1,44 @@
+<?php
+namespace Helper;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+class Mail
+{
+
+  public static function send($subject ,$message){
+
+    $mail = new PHPMailer(true);
+
+    try {
+
+        $mail->SMTPDebug  = false;
+        $mail->Host       = $_ENV['SMTP'];
+        $mail->SMTPAuth   = true;
+        $mail->Username   = $_ENV['SMTP_USER'];
+        $mail->Password   = $_ENV['SMTP_PASS'];
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port       = 587;
+        $mail->Subject    = $subject;
+        $mail->Body       = $message;
+        $mail->CharSet    = 'UTF-8';
+        $mail->Encoding   = 'base64';
+
+        $mail->isSMTP();
+        $mail->setFrom($_ENV['SMTP_USER'], 'Kuvvu GmbH');
+        $mail->addAddress($_ENV['EMAIL']);
+        $mail->isHTML(true);
+
+        $mail->send();
+
+    } catch (Exception $e) {
+
+       echo "Error: Mail \n";
+       print_r($e);
+
+    }
+
+  }
+
+}
